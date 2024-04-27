6bf22d2e0c7403516d0644d56c15e24dc332d0d7
Zakhar Borodayev <zborodayev@gmail.com>
2024-04-27 16:20:54 +0500

    new project

---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+export const Home = () => {
+  return (
+    <div>
+        <div className={styles.div}>
+            <h1 className={styles.h1}>Крестики нолики</h1>
+        </div>
+    </div>
+  )
+
+}
