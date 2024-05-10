e11f999931edda08bed1a36a64e0a00fe06d7a21
Zakhar Borodayev <zborodayev@gmail.com>
2024-05-10 21:06:07 +0500

    useState

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react'
-import styles from './Home.module.css'
+import { useState } from "react"
 
 export const Home = () => {
-    const [xod, setXod] = useState('')
-
-    const creatxod = e => {
-        console.log(xod)
-        e.preventDefault()
-    }
-
-    if (xod === 1) {
-      return {
-        
-      }
-    }
-
+  const [text, setText] = useState('')
+  const createText = (e) => {
+    e.preventDefault();
+    return <div>{text}</div>
+  } 
   return (
+  <div>
+    <input placeholder='Text' onChange={e => setText(() => e.target.value)} value={text}/>
+    <button onClick={createText}>create</button>
     
-    <div>
-        <div className={styles.div}>
-            <h1>Крестики нолики</h1>
-        </div>
-        <div>
-            <input className={styles.input} placeholder='1 - нолик  2 - крестик' onChange={e => setXod(e.target.value)} value={xod}/>
-            <button className={styles.button} onClick={creatxod}>походить</button>
-        
-        </div>
-    </div>
-)
-
+  </div>
+  )
 }
