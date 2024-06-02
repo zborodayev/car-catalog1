68f7fe34c56e9ebd9a83e7173bf4e5dc6b19bd7a
Zakhar Borodayev <zborodayev@gmail.com>
2024-06-02 16:31:54 +0500

    abc

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react"
-
+import styles from './Home.module.css'
 export const Home = () => {
-  const [text, setText] = useState('')
-  const createText = (e) => {
-    e.preventDefault();
-    return <div>{text}</div>
-  } 
+  const Buttons = (index) => {
+    return (
+      <button onClick={() =>  handleClick(index)} className={styles.button}></button>
+    )
+  }
+  const handleClick = (index) => {
+    console.log(`click`,index)
+  }
   return (
-  <div>
-    <input placeholder='Text' onChange={e => setText(() => e.target.value)} value={text}/>
-    <button onClick={createText}>create</button>
-    
-  </div>
+
+    <div>
+      <div className={styles.div}>
+      <h2>ход:</h2>
+        <div>
+          <Buttons index={1}></Buttons>
+          <Buttons index={2}></Buttons>
+          <Buttons index={3}></Buttons>
+        </div>
+        <div>
+          <Buttons index={4}></Buttons>
+          <Buttons index={5}></Buttons>
+          <Buttons index={6}></Buttons>
+        </div>
+        <div>
+          <Buttons index={7}></Buttons>
+          <Buttons index={8}></Buttons>
+          <Buttons index={9}></Buttons>
+        </div>
+      </div>
+    </div>
   )
 }
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Home} from './assets/components/Home/Home'
+import { Home } from "./assets/components/Home/Home"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
