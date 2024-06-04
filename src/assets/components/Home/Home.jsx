fb1a9dac62c79f0bb641bb0663fbac4f2f52c53b
Zakhar Borodayev <zborodayev@gmail.com>
2024-06-05 00:27:55 +0500

    a

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -1,35 +1,28 @@
+import { useState } from 'react'
 import styles from './Home.module.css'
+
+
+
 export const Home = () => {
-  const Buttons = (index) => {
-    return (
-      <button onClick={() =>  handleClick(index)} className={styles.button}></button>
-    )
-  }
-  const handleClick = (index) => {
-    console.log(`click`,index)
+  const [password, setPassword] = useState()
+  const createImg = () => {
+  
+  const Tpassword = 678912345
+  {password == Tpassword ? (
+    console.log('passwoed is correct')
+
+  )
+  : 
+  (
+    console.log('password is incorrect')
+  )}
   }
   return (
-
     <div>
-      <div className={styles.div}>
-      <h2>ход:</h2>
-        <div>
-          <Buttons index={1}></Buttons>
-          <Buttons index={2}></Buttons>
-          <Buttons index={3}></Buttons>
-        </div>
-        <div>
-          <Buttons index={4}></Buttons>
-          <Buttons index={5}></Buttons>
-          <Buttons index={6}></Buttons>
-        </div>
-        <div>
-          <Buttons index={7}></Buttons>
-          <Buttons index={8}></Buttons>
-          <Buttons index={9}></Buttons>
-        </div>
-      </div>
+      <input placeholder='password' onChange={e => setPassword(e.target.value)} value={password} />
+      <button onClick={createImg} className={styles.btn}>Submit</button>
     </div>
   )
 }
+
 
